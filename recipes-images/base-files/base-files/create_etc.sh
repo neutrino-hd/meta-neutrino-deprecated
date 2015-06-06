a58c1f4632e8dafb3261f64c54491cf5fa21abd5
MarkusVolk <f_l_k@gmx.net>
2015-06-06 12:12:24 +0200

    fix

--- recipes-images/base-files/base-files/create_etc.sh
+++ recipes-images/base-files/base-files/create_etc.sh
@@ -3,11 +3,14 @@ if [ -e GIT_URL ];then
 	exit
 else
 	cd /etc
+	if [ -e .git ];then fi
+	else
 	git config --system user.name "GIT_USER"
 	git config --system user.email "GIT_MAIL"
 	git config --system core.editor "nano"
 	git config --system http.sslverify false
 	etckeeper init
+	fi
 	mkdir -p GIT_URL
 	git init --bare GIT_URL
 	cd /etc && git remote add -f origin GIT_URL
