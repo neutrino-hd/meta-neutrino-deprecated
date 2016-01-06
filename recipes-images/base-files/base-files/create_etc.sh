21425cacc22ee8ede73c945ba057f2dc413c9d6b
MarkusVolk <f_l_k@t-online.de>
2016-01-06 10:29:27 +0100

    create_etc.sh:fix

--- recipes-images/base-files/base-files/create_etc.sh
+++ recipes-images/base-files/base-files/create_etc.sh
@@ -13,8 +13,8 @@ elif mountpoint -q $DEST;then
 	git config --system user.email "MAIL"
 	git config --system core.editor "nano"
 	git config --system http.sslverify false
+	fi	
 	etckeeper init
-	fi
 	mkdir -p $GIT__URL
 	git init --bare $GIT__URL
 	cd /etc && git remote add -f origin $GIT__URL
